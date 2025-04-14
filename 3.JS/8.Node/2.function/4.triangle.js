1ff734702444223f686076ce09028b44f4c542e3
Su-Hyun Park <lovehyun@hotmail.com>
2025-04-14 15:13:21 +0900

    node.js 외부모듈

--- 3.JS/8.Node/2.function/4.triangle.js
+++ 3.JS/8.Node/2.function/4.triangle.js
@@ -1,4 +1,4 @@
-const ROWS = 5; // 총 출력할 줄 수
+const ROWS = 8; // 총 출력할 줄 수
 
 function leftTriangle() { 
     let currentRow = 1;
@@ -15,3 +15,48 @@ function leftTriangle() {
 }
 
 leftTriangle();
+
+function leftTriangle2() {
+    for (let r = 1; r <= ROWS; r++) {
+        let stars = ""
+        for (let c = 1; c <= r; c++) {
+            // console.log('*');
+            stars += "*";
+        }
+        console.log(stars);
+    }
+    // console.log('**');
+    // console.log('***');
+    // console.log('****');
+    // console.log('*****');
+}
+
+console.log('---');
+leftTriangle2();
+
+function leftTriangle3(rows = 5) { // 함수의 인자를 받으면서 기본값 설정
+    for (let i = 1; i <= rows; i++) {
+        console.log('*'.repeat(i));
+    }
+}
+
+console.log('---');
+leftTriangle3();
+
+function invertedLeftTriangle3(rows = 5) { // 함수의 인자를 받으면서 기본값 설정
+    for (let i = rows; i >= 1; i--) {
+        console.log('*'.repeat(i));
+    }
+}
+
+console.log('---');
+invertedLeftTriangle3();
+
+function rightTriangle(rows = 5) {
+    for (let i = 1; i <= rows; i++) {
+        console.log(' '.repeat(rows-i) + '*'.repeat(i));
+    }
+}
+
+console.log('---');
+rightTriangle();
