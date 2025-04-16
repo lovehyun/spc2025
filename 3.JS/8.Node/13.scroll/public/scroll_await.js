c8e9774762ba5764f679ac555e1986f5a72231dc
Su-Hyun Park <lovehyun@hotmail.com>
2025-04-16 15:11:34 +0900

    무한스크롤 초안 커밋

--- 3.JS/8.Node/13.scroll/public/scroll_await.js
+++ 3.JS/8.Node/13.scroll/public/scroll_await.js
@@ -1,8 +1,12 @@
-console.log('로딩완료');
+const NUM_OF_ITEMS_PER_PAGE = 10;
+let start = 0;
+let end = start + NUM_OF_ITEMS_PER_PAGE;
+
+// console.log('로딩완료');
 // 미션1. 백엔드에 요청해서 데이터를 받아와서, 화면에 랜더링한다.
 async function loading() {
     // 미션1-1. 백엔드에 요청한다. fetch
-    const res = await fetch('/get-items')
+    const res = await fetch(`/get-items?start=${start}&end=${end}`)
     // 미션1-2. 데이터를 받아온다. res.xxxxx
     const data = await res.json();
     // 미션1-3. 화면에 렌더링한다. dom..xxxx
@@ -15,8 +19,23 @@ async function loading() {
         item.classList.add('item'); // 디자인 속성 추가
         myContainer.appendChild(item);
     })
+    // 오래된 돔을 찾아서 위에 지우기...
+
+    // 다음 로딩 준비...
+    start = end;
+    end += NUM_OF_ITEMS_PER_PAGE;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     loading();
+});
+
+window.addEventListener('scroll', () => {
+    // console.log('윈도우높이: ', window.innerHeight);
+    // console.log('스크롤위치: ', window.scrollY);
+    const endOfScroll = (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight);
+    console.log("화면끝? ", endOfScroll);
+    if (endOfScroll) {
+        loading();
+    }
 });
--- 3.JS/8.Node/13.scroll/server.js
+++ 3.JS/8.Node/13.scroll/server.js
@@ -18,9 +18,20 @@ app.use(express.static('public'));
 app.get('/get-items', (req, res) => {
     // 미션2. 원하는 갯수만큼만 보내주려면 어떻게 설계?? 
     //        입력 파라미터를 어떻게 정해야 할까??
-    // 미션2-1. 그래서, 난 어떻게 이 많은걸 나눌까~~~
-    // 미션2-2. 이걸 구현.
-    res.json(data);
+    // query 파라미터로 GET으로, start=10, end=20 라는 변수에 담아줄거다
+
+    // const start = req.query.start;
+    // const end = req.query.end;
+    const { start, end } = req.query;
+
+    // const userItems = [];
+    // for (let i = start; i < end; i++) {
+    //     userItems.push(data[i])
+    // }
+    // console.log(userItems);
+    const userItems = data.slice(start,end);
+
+    res.json(userItems);
 });
 
 app.listen(port, () => {
