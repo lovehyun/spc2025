04ac528acc2f262cda72503e7caeededdad6e8b6
Su-Hyun Park <lovehyun@hotmail.com>
2025-05-14 16:40:06 +0900

    rag 최종 완료

--- 5.GPT/PYTHON/9.RAG_web/app.py
+++ 5.GPT/PYTHON/9.RAG_web/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, send_from_directory
 import os
 
-from database import create_vector_db, answer_question
+from database2 import initialize_vector_db, create_vector_db, answer_question
 
 DATA_DIR = './DATA'
 
@@ -32,4 +32,5 @@ def ask_question():
     return jsonify({"answer":answer}), 200
 
 if __name__ == "__main__":
+    initialize_vector_db()
     app.run(debug=True)
