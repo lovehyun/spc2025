9aef6502173f8e98133f2ccc3432bc736c57f05e
Su-Hyun Park <lovehyun@hotmail.com>
2025-05-16 11:26:34 +0900

    python ai-gpt서버 개발중 (미완성)

--- 5.GPT/JS/7.todo_web/app.js
+++ 5.GPT/JS/7.todo_web/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const todoRoutes = require('./routes/todoRoutes');
-const chatbot = require('./routes/chatbot');
+const chatbot = require('./routes/chatbot_to_python');
 
 const app = express();
 const port = 3000;
